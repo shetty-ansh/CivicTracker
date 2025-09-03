732206b13a2d4a56c930d5b31d06ec09ce16f4f1
Ansh <ansh.shetty.22@gmail.com>
2025-09-04 02:31:17 +0530

    Frontend working

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ dotenv.config()
 const app = express();
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN || "http://localhost:4200",
     credentials: true
 }));
 
